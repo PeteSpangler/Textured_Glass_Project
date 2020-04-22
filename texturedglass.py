cadbe588cd599d029ae11bfe13d8d67b7bc38727
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-04-22 11:00:34 -0700

    Final Image

--- texturedglass.py
+++ texturedglass.py
@@ -35,7 +35,6 @@ def calculateVSegments(numberOfSegments, width):
     segmentWidth = int(width / numberOfSegments)
     for s in range(numberOfSegments):
         segments.append((segmentWidth * s, (segmentWidth * (s + 1)) - 1))
-    print (segments)
     return segments
 
 def vtranslate(segmentCount, img):
@@ -50,17 +49,23 @@ def vtranslate(segmentCount, img):
         copyImage[:imageHeight, segments[rightSegment][0]:segments[rightSegment][1]] = img[:imageHeight, segments[leftSegment][0]:segments[leftSegment][1]]
     return copyImage
 
-translatedImage = vtranslate(25, img)
+translatedImage = vtranslate(10, img)
 cv.imshow("BRO", translatedImage)
 cv.waitKey(0)
 cv.destroyAllWindows()
+# Step 5: generalize the function to take a number (such as 3) and the image, and do that amount of horizontal cuts with translation. 
+# Validate that the number is >= 3, and the number is < N/2, where N is the number of columns in the shape of the image. 
+# Then during the copying from source to destination, invert the locations of the columns from the outside in, skipping every other column and center columns. 
+# For example, image with columns [1,2,3] will end up [3,2,1], image with columns [1,2,3,4] will end up [4,2,3,1], 
+# image with columns [1,2,3,4,5] will end up [5,2,3,4,1], image with columns [1,2,3,4,5,6] will end up [6,2,4,3,5,1], and so on.
+# Step 6: call the function with parameters 3 and image, and show the result, it should be the same as in step 4.
+# Step 7: call the function with parameters 6 and image, and see that the result this as expected.
 
 def calculateHSegments(numberOfSegments, height):
     hsegments = []
     segmentHeight = int(height / numberOfSegments)
     for j in range(numberOfSegments):
         hsegments.append((segmentHeight * j, (segmentHeight * (j + 1)) - 1))
-    print (hsegments)
     return hsegments
 
 def htranslate(segmentCount, img):
@@ -75,24 +80,8 @@ def htranslate(segmentCount, img):
         copyImage2[hsegments[topsegment][0]:hsegments[topsegment][1], :imageWidth] = img[hsegments[bottomsegment][0]:hsegments[bottomsegment][1], :imageWidth]
     return copyImage2
 
-translatedImage2 = htranslate(25, img)
+translatedImage2 = htranslate(10, img)
 (cv.imshow("Bruv", translatedImage2))
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-
-
-# Step 5: generalize the function to take a number (such as 3) and the image, and do that amount of horizontal cuts with translation. 
-# Validate that the number is >= 3, and the number is < N/2, where N is the number of columns in the shape of the image. 
-# Then during the copying from source to destination, invert the locations of the columns from the outside in, skipping every other column and center columns. 
-# For example, image with columns [1,2,3] will end up [3,2,1], image with columns [1,2,3,4] will end up [4,2,3,1], 
-# image with columns [1,2,3,4,5] will end up [5,2,3,4,1], image with columns [1,2,3,4,5,6] will end up [6,2,4,3,5,1], and so on.
-# Step 6: call the function with parameters 3 and image, and show the result, it should be the same as in step 4.
-# Step 7: call the function with parameters 6 and image, and see that the result this as expected.
-
-
-rotateimage = np.rot90(img, 2)
-cv.imshow("Rotate", rotateimage)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
@@ -107,12 +96,49 @@ cv.destroyAllWindows()
 # [1',2,3',4',5,6'], and so on.
 # Step 11: call the function with parameters 3 and image, and show the result, it should be the same as in step 9.
 # Step 12: call the function with parameters 5 and image, and see that the result is as expected/
+def vflipud(segmentCount, img):
+    segments = calculateVSegments(segmentCount, imageWidth)
+    vflipudimage = np.flipud(np.copy(img))
+    for segment in range(int(segmentCount / 2) + 1):
+        if segment % 2 != 0:
+            continue
+        leftSegment = segment
+        rightSegment = segmentCount - 1 - segment
+        vflipudimage[:imageHeight, segments[leftSegment][0]:segments[leftSegment][1]] = img[:imageHeight, segments[rightSegment][0]:segments[rightSegment][1]]
+        vflipudimage[:imageHeight, segments[rightSegment][0]:segments[rightSegment][1]] = img[:imageHeight, segments[leftSegment][0]:segments[leftSegment][1]]
+    return vflipudimage
+
+flippedandchopped = vflipud(5, img)
+(cv.imshow("quoi", flippedandchopped))
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+def hflip(segmentCount, img):
+    hsegments = calculateHSegments(segmentCount, imageHeight)
+    hflipimage = np.fliplr(np.copy(img))
+    for hsegment in range(int(segmentCount / 2) + 1):
+        if hsegment % 2 != 0:
+            continue
+        bottomsegment = hsegment
+        topsegment = segmentCount - 1 - hsegment
+        hflipimage[hsegments[bottomsegment][0]:hsegments[bottomsegment][1], :imageWidth] = img[hsegments[topsegment][0]:hsegments[topsegment][1], :imageWidth]
+        hflipimage[hsegments[topsegment][0]:hsegments[topsegment][1], :imageWidth] = img[hsegments[bottomsegment][0]:hsegments[bottomsegment][1], :imageWidth]
+    return hflipimage
+
+
+flipimage = hflip(5, img)
+cv.imshow("Flipped", flipimage)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
 # Chapter 4: the final output
 # Step 13: load an image from a file and combine a call to the translation function with a call to the rotation function. Rotate 3 times and translate 5.
 # Step 14: show the resulting image.
 # Step 15: profit!
-
+FinalImage = vflipud(5, (vtranslate(5, img)))
+cv.imshow("Final", FinalImage)
+cv.waitKey(0)
+cv.destroyAllWindows()
 # Shoot me with any questions, I expect many.
 
 # def translateDavid(inputImage, segmentCount):
